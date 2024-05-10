578c691d97045c26506a50c1d0c22df7703eb260
Gandharv Talikoti <gandharvtalikoti19@gmail>
2024-05-10 16:21:03 +0530

    Show issue details

---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LoadingIssueDetail = () => {
+  return (
+    <div>loading..............</div>
+  )
+}
+
+export default LoadingIssueDetail
---
+++ app/issues/new/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewIssuePageLoading = () => {
+  return (
+    <div>Loading...</div>
+  )
+}
+
+export default NewIssuePageLoading
