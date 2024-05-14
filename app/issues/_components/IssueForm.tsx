86abddb83814d58516c375a40151515a2b9e1381
Gandharv Talikoti <gandharvtalikoti19@gmail>
2024-05-14 10:49:25 +0530

    Removed Duplicate skeletons for good ux

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -43,7 +43,7 @@ const IssueForm = ({ issue }: Props) => {
       setSubmitting(true);
       if (issue) await axios.put("/api/issues/" + issue.id, data);
       else await axios.post("/api/issues", data);
-      router.push("/issues");
+      router.push("/issues/list");
       router.refresh();// this tells nextjs to refresh the content of the current route
     } catch (error) {
       setSubmitting(false);
