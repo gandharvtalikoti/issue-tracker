9b11b29551c9247888b6a0e6565740fdd1d30089
Gandharv Talikoti <gandharvtalikoti19@gmail>
2024-05-16 01:10:56 +0530

    Populate the assignee select component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,15 +1,37 @@
-import { Select } from "@radix-ui/themes";
-import React from "react";
+"use client";
+import { User } from "@prisma/client";
+import { Avatar, Select } from "@radix-ui/themes";
+import axios from "axios";
+import React, { use, useEffect, useState } from "react";
 
 const AssigneeSelect = () => {
+  // now to populate select users with the users in the database
+  // we have to build an API endpoitn because this is a CLIENT componenent
+  // and we CANNOT ACCESS PRISMA ON CLIENT COMPONENT
+  // PRISMA IS ONLY AVAILABLE ON SERVER
+
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await axios.get<User[]>("/api/users"); // Make sure the response is an array
+      setUsers(data);
+    };
+    fetchUsers();
+  }, []);
+
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="1">Orange</Select.Item>
-          <Select.Item value="2">Apple</Select.Item>
+          {users.map((user) => (
+            <Select.Item  key={user.id} value={user.id}>
+            
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
