285f3949b7348d8c74515e6a0399c44276e23bc4
Gandharv Talikoti <gandharvtalikoti19@gmail>
2024-05-15 20:52:13 +0530

    corrected typo in .env

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -11,9 +11,9 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-  session: {
-    strategy: 'jwt',
-  },
+ session:{
+  strategy:'jwt'
+ }
 });
 
 export { handler as GET, handler as POST };
