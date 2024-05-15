d4fe303b6ca55f552188ea40a50c44a218d22940
Gandharv Talikoti <gandharvtalikoti19@gmail>
2024-05-15 23:52:28 +0530

    Refactor the NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -14,17 +14,6 @@ import { usePathname } from "next/navigation";
 import { FaBug } from "react-icons/fa";
 
 const NavBar = () => {
-  const currPath = usePathname();
-  // to add login and logout links we have to use useSession() hook provided by next auth
-  const { status, data: session } = useSession();
-  // staus - which can me authenticated | unauthenticated | loading
-  // data: session -  with this we can get name, email and image
-
-  const links = [
-    { label: "Dashboard", href: "/" },
-    { label: "Issues", href: "/issues/list" },
-  ];
-
   return (
     <nav className="border-b mb-5 px-5 py-3">
       <Container>
@@ -32,59 +21,81 @@ const NavBar = () => {
           <Flex align="center" gap="3">
             <Link href="/">
               {" "}
-              <FaBug />{" "}
+              <FaBug />
             </Link>
-            <ul className="flex space-x-6">
-              {links.map((link) => (
-                <li key={link.href}>
-                  <Link
-                    className={classnames({
-                      "text-white-900": link.href === currPath,
-                      "text-zinc-400": link.href !== currPath,
-                      "hover:text-white-500 transition-colors": true,
-                    })}
-                    href={link.href}
-                  >
-                    {link.label}
-                  </Link>
-                </li>
-              ))}
-            </ul>
+            <NavLinks />
           </Flex>
-          <Box>
-            {status === "authenticated" && (
-              // <Link href="/api/auth/signout">Log out</Link>
-              <DropdownMenu.Root>
-                <DropdownMenu.Trigger>
-                  <Avatar
-                    src={session.user!.image!}
-                    fallback="?"
-                    size="2"
-                    radius="full"
-                    className="cursor-pointer"
-                    referrerPolicy="no-referrer"
-                  />
-                </DropdownMenu.Trigger>
-                <DropdownMenu.Content>
-                  <Text size="2">
-                    <DropdownMenu.Label>
-                      {session.user!.email}
-                    </DropdownMenu.Label>
-                    <DropdownMenu.Item>
-                    <Link href="/api/auth/signout">Log out</Link>
-                    </DropdownMenu.Item>
-                  </Text>
-                </DropdownMenu.Content>
-              
-              </DropdownMenu.Root>
-            )}
-            {status === "unauthenticated" && (
-              <Link href="/api/auth/signin">Log in</Link>
-            )}
-          </Box>
+          <AuthStaus />
         </Flex>
       </Container>
     </nav>
+  );
+};
+
+const NavLinks = () => {
+  const currPath = usePathname();
+
+  const links = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issues", href: "/issues/list" },
+  ];
+
+  return (
+    <ul className="flex space-x-6">
+      {links.map((link) => (
+        <li key={link.href}>
+          <Link
+            className={classnames({
+              "nav-link": true,
+              "!text-zinc-900": link.href === currPath,
+            })}
+            href={link.href}
+          >
+            {link.label}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const AuthStaus = () => {
+  // to add login and logout links we have to use useSession() hook provided by next auth
+  const { status, data: session } = useSession();
+  // staus - which can me authenticated | unauthenticated | loading
+  // data: session -  with this we can get name, email and image
+
+  if (status === "loading") {
+    return null;
+  }
+  if (status == "unauthenticated") {
+    return <Link className="nav-link" href="/api/auth/signin">Log in</Link>;
+  }
+
+  return (
+    <Box>
+      <Link href="/api/auth/signout">Log out</Link>
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <Avatar
+            src={session!.user!.image!}
+            fallback="?"
+            size="2"
+            radius="full"
+            className="cursor-pointer"
+            referrerPolicy="no-referrer"
+          />
+        </DropdownMenu.Trigger>
+        <DropdownMenu.Content>
+          <Text size="2">
+            <DropdownMenu.Label>{session!.user!.email}</DropdownMenu.Label>
+            <DropdownMenu.Item>
+              <Link href="/api/auth/signout">Log out</Link>
+            </DropdownMenu.Item>
+          </Text>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </Box>
   );
 };
 
