24997141f67bc6c8c0f9c4357c7f5e0274ca3453
Gandharv Talikoti <gandharvtalikoti19@gmail>
2024-05-16 11:04:37 +0530

    Fetch data with react query (cache)

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,24 +1,43 @@
 "use client";
+import { Skeleton } from "@/app/components";
 import { User } from "@prisma/client";
 import { Avatar, Select } from "@radix-ui/themes";
+import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import React, { use, useEffect, useState } from "react";
 
 const AssigneeSelect = () => {
   // now to populate select users with the users in the database
-  // we have to build an API endpoitn because this is a CLIENT componenent
+  // we have to build an API endpoint because this is a CLIENT componenent
   // and we CANNOT ACCESS PRISMA ON CLIENT COMPONENT
   // PRISMA IS ONLY AVAILABLE ON SERVER
 
-  const [users, setUsers] = useState<User[]>([]);
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery<User[]>({
+    queryKey: ["users"],
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
+    staleTime: 60 * 100, // 60s
+    retry: 3,
+  });
+  if (isLoading) return <Skeleton />;
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const { data } = await axios.get<User[]>("/api/users"); // Make sure the response is an array
-      setUsers(data);
-    };
-    fetchUsers();
-  }, []);
+  if (error) return null;
+
+  // this way of fetching data is not optimal as it will not be stored in cache
+  // it will have to make request everytime when user vistis this page
+  // and that is the reason why we will use React Query to fetch data
+
+  // const [users, setUsers] = useState<User[]>([]);
+  // useEffect(() => {
+  //   const fetchUsers = async () => {
+  //     const { data } = await axios.get<User[]>("/api/users"); // Make sure the response is an array
+  //     setUsers(data);
+  //   };
+  //   fetchUsers();
+  // }, []);
 
   return (
     <Select.Root>
@@ -26,9 +45,8 @@ const AssigneeSelect = () => {
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          {users.map((user) => (
-            <Select.Item  key={user.id} value={user.id}>
-            
+          {users?.map((user) => (
+            <Select.Item key={user.id} value={user.id}>
               {user.name}
             </Select.Item>
           ))}
