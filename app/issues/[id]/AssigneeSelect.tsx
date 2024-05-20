e6deb8d143857f96f6ea1631db09c37749a63980
Gandharv Talikoti <gandharvtalikoti19@gmail>
2024-05-20 13:38:51 +0530

    Refactor assignee select component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -7,6 +7,10 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
 
+// now to populate select users with the users in the database
+// we have to build an API endpoint because this is a CLIENT componenent
+// and we CANNOT ACCESS PRISMA ON CLIENT COMPONENT
+// PRISMA IS ONLY AVAILABLE ON SERVER
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const { data: users, error, isLoading } = useUsers();
 
@@ -17,12 +21,25 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const assignIssue = (userId: string) => {
     axios
       .patch("/api/issues/" + issue.id, {
-        assignedToUserId: userId || null,
+        assignedToUserId: userId === "null" ? null : userId,
       })
       .catch(() => {
         toast.error("Changes could not be saved.");
       });
   };
+
+  // this way of fetching data is not optimal as it will not be stored in cache
+  // it will have to make request everytime when user vistis this page
+  // and that is the reason why we will use React Query to fetch data
+
+  // const [users, setUsers] = useState<User[]>([]);
+  // useEffect(() => {
+  //   const fetchUsers = async () => {
+  //     const { data } = await axios.get<User[]>("/api/users"); // Make sure the response is an array
+  //     setUsers(data);
+  //   };
+  //   fetchUsers();
+  // }, []);
 
   return (
     <>
@@ -34,7 +51,7 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
         <Select.Content>
           <Select.Group>
             <Select.Label>Suggestions</Select.Label>
-            <Select.Item value="s">Unassigned</Select.Item>
+            <Select.Item value="null">Unassigned</Select.Item>
             {users?.map((user) => (
               <Select.Item key={user.id} value={user.id}>
                 {user.name}
@@ -51,8 +68,7 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
 const useUsers = () =>
   useQuery<User[]>({
     queryKey: ["users"],
-    queryFn: () =>
-      axios.get("/api/users").then((res) => res.data),
+    queryFn: () => axios.get("/api/users").then((res) => res.data),
     staleTime: 60 * 1000, //60s
     retry: 3,
   });
