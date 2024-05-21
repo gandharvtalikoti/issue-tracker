3a735f923c60baa3d7f3ba35dd18b4fd8432e592
Gandharv Talikoti <gandharvtalikoti19@gmail>
2024-05-21 14:08:20 +0530

    Fix Filtering Bugs

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -1,39 +1,48 @@
 "use client";
-import { Status } from '@prisma/client'
-import { Select } from '@radix-ui/themes'
-import { useRouter } from 'next/navigation'
+import { Status } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const statuses: { label: string; value?: Status }[] = [
-  { label: 'All' },
-  { label: 'Open', value: 'OPEN' },
-  { label: 'In Progress', value: 'IN_PROGRESS' },
-  { label: 'Closed', value: 'COMPLETED' },
-]
+  { label: "All" },
+  { label: "Open", value: "OPEN" },
+  { label: "In Progress", value: "IN_PROGRESS" },
+  { label: "Closed", value: "COMPLETED" },
+];
 
 const IssueStatusFilter = () => {
-  const router = useRouter()
+  const router = useRouter();
+  const searchParams = useSearchParams();
 
   return (
     <Select.Root
+      defaultValue={searchParams.get("status") || ""}
       onValueChange={(status) => {
         // query paramenters
-        const query = status === 'ALL' ? '' : `?status=${status}`
-        router.push(`/issues/list${query}`) // this will redirect the user
-      }}  
+
+        // dynamically arrangement if params
+        const params = new URLSearchParams(); // we can add key value pairs in this dynamically
+        // then convert it into string
+
+        if (status) params.append("status", status);
+        if (searchParams.get("orderBy"))
+          params.append("orderBy", searchParams.get("orderBy")!);
+
+        const query = params.size ? "?" + params.toString() : "";
+        // const query = status === "ALL" ? "" : `?status=${status}`;
+        router.push(`/issues/list${query}`); // this will redirect the user
+      }}
     >
-      <Select.Trigger placeholder='Filter by status...' />
+      <Select.Trigger placeholder="Filter by status..." />
       <Select.Content>
         {statuses.map((status) => (
-          <Select.Item
-            key={status.value}
-            value={status.value ?? 'ALL'}
-          >
+          <Select.Item key={status.value} value={status.value ?? "ALL"}>
             {status.label}
           </Select.Item>
         ))}
       </Select.Content>
     </Select.Root>
-  )
-}
+  );
+};
 
-export default IssueStatusFilter
+export default IssueStatusFilter;
