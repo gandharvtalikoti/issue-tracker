ada1423fbad82280a9c9b016406e09efaa810a20
Gandharv Talikoti <gandharvtalikoti19@gmail>
2024-05-21 18:57:19 +0530

    Implement Pagination

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,37 +1,74 @@
-import {
-  ChevronLeftIcon,
-  ChevronRightIcon,
-  DoubleArrowLeftIcon,
-  DoubleArrowRightIcon,
-} from "@radix-ui/react-icons";
-import { Button, Flex, Text } from "@radix-ui/themes";
-import React from "react";
+'use client';
+
+import { ChevronLeftIcon, ChevronRightIcon, DoubleArrowLeftIcon, DoubleArrowRightIcon } from '@radix-ui/react-icons';
+import { Button, Flex, Text } from '@radix-ui/themes';
+import { useRouter, useSearchParams } from 'next/navigation';
+
 interface Props {
-  // total number of items
+      // total number of items
   itemCount: number;
-  //the number of items to show on each page (pageSize)
+    //the number of items to show on each page (page Size)
   pageSize: number;
-  //current page
+    //current page
   currentPage: number;
 }
-const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
+
+const Pagination = ({
+  itemCount,
+  pageSize,
+  currentPage,
+}: Props) => {
+      // we use this hook useRouter to update the current URL
+  const router = useRouter();
+
+    // we useSearchParams to access the current query parameters
+  const searchParams = useSearchParams();
+
   const pageCount = Math.ceil(itemCount / pageSize);
   if (pageCount <= 1) return null;
+
+   // function for updating current page
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('page', page.toString());
+    router.push('?' + params.toString());
+  }
+
   return (
     <Flex align="center" gap="2">
       <Text size="2">
         Page {currentPage} of {pageCount}
       </Text>
-      <Button variant="soft" color="gray" disabled={currentPage === 1}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => changePage(1)}
+      >
         <DoubleArrowLeftIcon />
       </Button>
-      <Button variant="soft" color="gray" disabled={currentPage === 1}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => changePage(currentPage - 1)}
+      >
         <ChevronLeftIcon />
       </Button>
-      <Button variant="soft" color="gray" disabled={currentPage === pageCount}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(currentPage + 1)}
+      >
         <ChevronRightIcon />
       </Button>
-      <Button variant="soft" color="gray" disabled={currentPage === pageCount}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(pageCount)}
+      >
         <DoubleArrowRightIcon />
       </Button>
     </Flex>
--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,18 @@ import Pagination from "./components/Pagination";
 import { Button } from "@radix-ui/themes";
 import { DoubleArrowLeftIcon } from "@radix-ui/react-icons";
 
-export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   return (
     <>
-      <Pagination itemCount={100} pageSize={10} currentPage={2} />
-      
+      <Pagination
+        itemCount={100}
+        pageSize={10}
+        currentPage={parseInt(searchParams.page)}
+      />
     </>
   );
 }
