b25817905f5718bb39f52a35e7b1d169f9b35a58
Gandharv Talikoti <gandharvtalikoti19@gmail>
2024-05-21 15:39:03 +0530

    Build the layout of the pagenation component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Pagination from "./components/Pagination";
+import { Button } from "@radix-ui/themes";
+import { DoubleArrowLeftIcon } from "@radix-ui/react-icons";
 
 export default function Home() {
   return (
     <>
-      <h1>Hello World</h1>
+      <Pagination itemCount={100} pageSize={10} currentPage={2} />
+      
     </>
   );
 }
