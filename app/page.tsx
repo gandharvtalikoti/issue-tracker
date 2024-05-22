982373cade036f4c3b5d0a7ab4ec1548e92cff5a
Gandharv Talikoti <gandharvtalikoti19@gmail>
2024-05-22 13:45:34 +0530

    Build the latest issue component

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,12 @@ import Image from "next/image";
 import Pagination from "./components/Pagination";
 import { Button } from "@radix-ui/themes";
 import { DoubleArrowLeftIcon } from "@radix-ui/react-icons";
+import LastestIssues from "./LastestIssues";
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
+export default function Home() {
   return (
     <>
-      <Pagination
-        itemCount={100}
-        pageSize={10}
-        currentPage={parseInt(searchParams.page)}
-      />
+      <LastestIssues />
     </>
   );
 }
